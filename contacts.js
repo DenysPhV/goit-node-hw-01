17261eb055452c5616c6cf48d678efff04044ba9
Denys Filichkin <denys.philichkin@gmail.com>
2021-12-11 21:13:13 +0200

    fix

--- contacts.js
+++ contacts.js
@@ -8,71 +8,53 @@ const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
 //node index.js --action list
 async function listContacts() {
-  try {
-    const data = await fs.readFile(contactsPath);
-    const contactsList = JSON.parse(data);
-    return contactsList;
-  } catch (error) {
-    throw error;
-  }
+  const data = await fs.readFile(contactsPath);
+  const contactsList = JSON.parse(data);
+  return contactsList;
 }
 
 // node index.js --action get --id 5
 async function getContactById(contactId) {
-  try {
-    const contactsList = await listContacts();
-    const findContact = contactsList.find((item) => item.id === contactId);
+  const contactsList = await listContacts();
+  const findContact = contactsList.find((item) => item.id === contactId);
 
-    if (!findContact) {
-      throw new Error(`Contact with id = ${contactId} not found`);
-    }
-    return findContact;
-  } catch (error) {
-    throw error;
+  if (!findContact) {
+    throw new Error(`Contact with id = ${contactId} not found`);
   }
+  return findContact;
 }
 
 //node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
 async function addContact(name, email, phone) {
-  try {
-    const contactsList = await listContacts();
-    const checkID = () =>
-      contactsList.some((item) => item.id === Id) ? checkID(id + 1) : id;
+  const contactsList = await listContacts();
+  const checkID = () =>
+    contactsList.some((item) => item.id === Id) ? checkID(id + 1) : id;
 
-    const newContact = {
-      id: v4(),
-      name,
-      email,
-      phone,
-    };
-    contactsList.push(newContact);
+  const newContact = {
+    id: v4(),
+    name,
+    email,
+    phone,
+  };
+  contactsList.push(newContact);
 
-    await fs.writeFile(contactsPath, JSON.stringify(contactsList));
-    return newContact;
-  } catch (error) {
-    throw error;
-  }
+  await fs.writeFile(contactsPath, JSON.stringify(contactsList));
+  return newContact;
 }
 
 // node index.js --action remove --id=3
 async function removeContact(contactId) {
-  try {
-    const contactsList = await listContacts();
-    const contactIndex = contactsList.findIndex(
-      (item) => item.id === contactId,
-    );
+  const contactsList = await listContacts();
+  const contactIndex = contactsList.findIndex((item) => item.id === contactId);
 
-    if (contactIndex === -1) {
-      throw new Error(`Contact with id = ${contactId} not found`);
-    }
-
-    const deleteContact = await contactsList.splice(contactIndex, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(contactsList));
-
-    return deleteContact;
-  } catch (error) {
-    throw error;
+  if (contactIndex === -1) {
+    throw new Error(`Contact with id = ${contactId} not found`);
   }
+
+  const deleteContact = await contactsList.splice(contactIndex, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contactsList));
+
+  return deleteContact;
 }
 
 module.exports = { listContacts, getContactById, removeContact, addContact };
